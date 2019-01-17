fd9915e99cabcab2415d647f247a3335f841e813
Irinakir <kirillova_irina@ukr.net>
2019-01-17 17:45:58 +0200

    Updates

---
+++ precache-manifest.5ebc9f31b52bfe8d99194edec385d302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b659f8265b4543736dca",
+    "url": "/react_project/weather/static/js/runtime~main.b659f826.js"
+  },
+  {
+    "revision": "406dc1c1972e719cf923",
+    "url": "/react_project/weather/static/js/main.406dc1c1.chunk.js"
+  },
+  {
+    "revision": "1ccefadcb3820da12fc3",
+    "url": "/react_project/weather/static/js/1.1ccefadc.chunk.js"
+  },
+  {
+    "revision": "406dc1c1972e719cf923",
+    "url": "/react_project/weather/static/css/main.c46f274f.chunk.css"
+  },
+  {
+    "revision": "1ccefadcb3820da12fc3",
+    "url": "/react_project/weather/static/css/1.e80deb83.chunk.css"
+  },
+  {
+    "revision": "de8934b460af90b37cea95bfd128719c",
+    "url": "/react_project/weather/index.html"
+  }
+];
