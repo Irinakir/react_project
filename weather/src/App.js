0cbc839482323b1a800e5fefc17ce776d2c0b6a4
Irinakir <kirillova_irina@ukr.net>
2019-01-18 17:12:07 +0200

    weather

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/dist/litera/bootstrap.css";
-
+import "mdbootstrap/css/bootstrap.css";
+//import "mdbreact/dist/css/mdb.css";
 //import "bootstrap/dist/css/bootstrap.css";
 //import Nav from "react-bootstrap/lib/Nav";
 //import Navbar from "react-bootstrap/lib/Navbar";
@@ -10,6 +12,8 @@ import "bootswatch/dist/litera/bootstrap.css";
 //import Grid  from "react-bootstrap/lib/Grid";
 //import Row  from "react-bootstrap/lib/Row";
 //import Col  from "react-bootstrap/lib/Col";
+
+//import {Navbar, Nav, NavItem, Grid, Row, Col} from 'mdbreact';
 import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 
 const PLACES = [
@@ -25,6 +29,7 @@ const PLACES = [
   { namer: "Черкасы", name: "Cherkasy" }
 
 ];
+
 
 class WeatherDisplay extends Component {
   constructor() {
@@ -47,9 +52,9 @@ class WeatherDisplay extends Component {
     if (!weatherData) return <div>Проверяем информацию ....</div>;
     const weather = weatherData.weather[0];
     const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
-  //  const temperat= Math.round((weatherData.main.temp - 32) / 1.8);
-  //  const temperat_max= Math.round((weatherData.main.temp_max - 32) / (5/9));
-  //  const temperat_min= Math.round((weatherData.main.temp_min - 32) / (5/9));
+   const temperat= Math.round(weatherData.main.temp);
+   const temperat_max= Math.round(weatherData.main.temp_max);
+   const temperat_min= Math.round(weatherData.main.temp_min);
  //const speed_wind= Math.round(0.44704*(weatherData.wind.speed));
     return (
 
@@ -59,10 +64,10 @@ class WeatherDisplay extends Component {
           <img src={iconUrl} alt={weatherData.description} />
         </h1>
 
-        <p>Сейчас погода: {weatherData.main.temp}°</p>
-        <p>Максилальная темп.:  {weatherData.main.temp_max}°</p>
-        <p>Минимальная темп.: {weatherData.main.temp_min}°</p>
-        <p>Скорость ветра:  {weatherData.wind.speed} mi/hr</p>
+        <p>Сейчас погода: {temperat}°</p>
+        <p>Максилальная темп.:  { temperat_max}°</p>
+        <p>Минимальная темп.: {temperat_min}°</p>
+        <p>Скорость ветра:  {weatherData.wind.speed} м/с</p>
       </div>
     );
   }
@@ -111,5 +116,60 @@ class App extends Component {
     );
   }
 }
+
+class NewButton extends React.Component {
+  render() {
+     const url = 'App_cal.js';
+    return (
+
+      //<button onclick="window.open.href='/App_cal'">Continue</button>
+     //<a className="mybutton" href="App_cal.js"> Календарь </a>
+    <a className="mybutton" href={url} target="_blank" rel="noopener noreferrer"> Календарь </a>
+      //  <button onclick= "window.open(url,  '_blank')">Continue</button>
+    //  <button className="mybutton" onClick={() => window.open(url, '_blank')}>
+    //    Календарь
+    //  </button>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <NewButton />,
+  document.getElementById('buttonroot')
+);
+
+
+const dz1=<a href="https://irinakir.github.io/" target="_blank"  rel="noopener noreferrer">1 работа</a>;
+const dz2=<a href="https://irinakir.github.io/site.github.io/" target="_blank"  rel="noopener noreferrer">2 работа</a>;
+const dz3=<a href="https://irinakir.github.io/web_project/" target="_blank" rel="noopener noreferrer">3 работа</a>;
+const dz4=<a href="https://irinakir.github.io/site.github.io/curriculum_vitae.html" target="_blank"  rel="noopener noreferrer">CV</a>;
+
+const numbers = [dz1, dz2, dz3, dz4];
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    // Correct! Key should be specified inside the array.
+    <ListItem key={number.toString()}
+              value={number} />
+
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
+ReactDOM.render(
+  <NumberList numbers={numbers}/>,
+  document.getElementById('linkroot')
+);
+
 
 export default App;
